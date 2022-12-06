ee531afed9e34e4bd29f7362ab9cb41ddb03a40d
knowansaritra <aritrakunda3@gmail.com>
2022-12-06 23:49:45 +0530

    added point feature, added more styling

--- external.js
+++ external.js
@@ -1,8 +1,8 @@
 const rock = document.getElementById('rock');
 const paper = document.getElementById('paper');
 const scissors = document.getElementById('scissors');
-
-
+let you= 0;
+let computer = 0;
 
 if(rock.addEventListener("click",()=>{
     player = 0;
@@ -27,31 +27,171 @@ const computerPlays =()=>{
 }
 
 const gamePlay = (playerChoose, computerChoose)=>{
+    let x=document.getElementById("computer").textContent;
+    let y=document.getElementById("you").textContent;
+    let takenStringX="";
+    let takenStringY="";
+    //0===rock
+    //1===paper
+    //2===scissors
         if(playerChoose==computerChoose)
-        alert("draw");
+        {
+    //    alert("draw");
+        you+=1;
+        computer+=1;
+     
+        for(let i=0; i<x.length; i++)
+        {
+            takenStringX=takenStringX+x.charAt(i);
+            if(x.charAt(i)==="=")
+            {
+                break;
+            }
+        }
+        for(let i=0; i<y.length; i++)
+        {
+            takenStringY=takenStringY+y.charAt(i);
+            if(y.charAt(i)==="=")
+            {
+                break;
+            }
+        }
+        document.getElementById("computer").textContent=takenStringX+" "+computer;
+        document.getElementById("you").textContent=takenStringY+" "+you;
+        }
         else if(playerChoose==0 && computerChoose==1)
         {
-        alert("computer wins");
+     //   alert("player wins");
+            computer+=1;
+        //player+=1;
+            for(let i=0; i<x.length; i++)
+            {
+                takenStringX=takenStringX+x.charAt(i);
+                if(x.charAt(i)==="=")
+                {
+                    break;
+                }
+            }
+            for(let i=0; i<y.length; i++)
+            {
+                takenStringY=takenStringY+y.charAt(i);
+                if(y.charAt(i)==="=")
+                {
+                    break;
+                }
+            } 
+            document.getElementById("computer").textContent=takenStringX+" "+computer;   
+   //    let yourPoints = document.getElementById("you");
         }
         else if(playerChoose==0 && computerChoose==2)
         {
-            alert("player wins");
+        //    alert("player wins");
+          you+=1;
+          for(let i=0; i<x.length; i++)
+          {
+              takenStringX=takenStringX+x.charAt(i);
+              if(x.charAt(i)==="=")
+              {
+                  break;
+              }
+          }
+          for(let i=0; i<y.length; i++)
+          {
+              takenStringY=takenStringY+y.charAt(i);
+              if(y.charAt(i)==="=")
+              {
+                  break;
+              }
+          } 
+          document.getElementById("you").textContent=takenStringY+" "+you;
         }
         else if(playerChoose==1 && computerChoose==0)
         {
-            alert("computer wins");
+        //    alert("computer wins");
+        you+=1;
+        for(let i=0; i<x.length; i++)
+        {
+            takenStringX=takenStringX+x.charAt(i);
+            if(x.charAt(i)==="=")
+            {
+                break;
+            }
+        }
+        for(let i=0; i<y.length; i++)
+        {
+            takenStringY=takenStringY+y.charAt(i);
+            if(y.charAt(i)==="=")
+            {
+                break;
+            }
+        } 
+        document.getElementById("you").textContent=takenStringY+" "+you;   
         }
         else if(playerChoose==1 && computerChoose==2)
         {
-            alert("computer wins");
+            //alert("computer wins");
+            computer+=1;
+            for(let i=0; i<x.length; i++)
+            {
+                takenStringX=takenStringX+x.charAt(i);
+                if(x.charAt(i)==="=")
+                {
+                    break;
+                }
+            }
+            for(let i=0; i<y.length; i++)
+            {
+                takenStringY=takenStringY+y.charAt(i);
+                if(y.charAt(i)==="=")
+                {
+                    break;
+                }
+            } 
+            document.getElementById("computer").textContent=takenStringX+" "+computer;   
         }
         else if(playerChoose==2 && computerChoose==0)
         {
-            alert("computer wins");
+            //alert("computer wins");
+            computer+=1;
+            for(let i=0; i<x.length; i++)
+            {
+                takenStringX=takenStringX+x.charAt(i);
+                if(x.charAt(i)==="=")
+                {
+                    break;
+                }
+            }
+            for(let i=0; i<y.length; i++)
+            {
+                takenStringY=takenStringY+y.charAt(i);
+                if(y.charAt(i)==="=")
+                {
+                    break;
+                }
+            } 
+            document.getElementById("computer").textContent=takenStringX+" "+computer;   
         }
         else if(playerChoose==2 && computerChoose==1)
         {
-            alert("player wins");
+            //alert("player wins");
+            you+=1;
+            for(let i=0; i<x.length; i++)
+            {
+                takenStringX=takenStringX+x.charAt(i);
+                if(x.charAt(i)==="=")
+                {
+                    break;
+                }
+            }
+            for(let i=0; i<y.length; i++)
+            {
+                takenStringY=takenStringY+y.charAt(i);
+                if(y.charAt(i)==="=")
+                {
+                    break;
+                }
+            } 
+            document.getElementById("you").textContent=takenStringY+" "+you;   
         }
 
 }
